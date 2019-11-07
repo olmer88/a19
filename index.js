3ced8aae3ef914b542d2499274d20289919feb24
max <maksym.zhuk@ria.com>
2019-11-07 09:41:13 +0200

    todo app

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const Koa = require('koa');
-const app = new Koa();
+const render = require('koa-ejs');
+const path = require('path');
 
-app.use(ctx => {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.body = 'Hello Koa';
+
+const app = new Koa();
+render(app, {
+  root: path.join(__dirname, 'view'),
+  layout: false,
+  viewExt: 'html',
+  cache: false,
+  debug: false,
+});
+
+app.use(async (ctx, next) => {
+  await ctx.render('index');
 });
 
 app.listen(8080);
